b642a8ecf92d21f3da9b6e5070892b999206a78e
Gabe-Matherley <157724600+Gabe-Matherley@users.noreply.github.com>
2024-02-01 16:41:30 -0600

    update main.py2

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 #REMOVE PASS AND FIX THE FUNCTION
 def sum_of_products(list1, list2):
-    pass
+    import math 
+    if len(list1) != len(list2):
+        return 'error'
+    else:
+        for a, b in zip(list1, list2):
+            sum_of_products.append(a * b)
+
+
+
+
 
 if __name__ == '__main__':
    #REMOVE PASS AND YOUR CODE GOES HERE
-    pass
+    print(sum_of_products)
