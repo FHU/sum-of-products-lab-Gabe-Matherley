8321577054a75dd722b29fcc8df4d412b73ce224
Gabe-Matherley <157724600+Gabe-Matherley@users.noreply.github.com>
2024-02-09 19:01:05 -0600

    update 6
    seeing if error is the error

--- main.py
+++ main.py
@@ -2,15 +2,12 @@
 #change to test push and forks
 def sum_of_products(list1, list2):
     import math 
-    if len(list1) != len(list2):
-        return 'error'
-    else:
-        products = []
-        list1 = list1.replace(' ','')
-        list2 = list2.replace(' ','')
-        for a, b in zip(list1, list2):
-            products.append(int(a) * int(b))
-        return sum(products)
+    products = []
+    list1 = list1.replace(' ','')
+    list2 = list2.replace(' ','')
+    for a, b in zip(list1, list2):
+        products.append(int(a) * int(b))
+    return sum(products)
 
 
 
